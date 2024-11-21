e3c7e7bee08fd95886814b283040a5415e131400
shvam0000 <shvam0000@gmail.com>
2024-11-20 19:05:14 -0500

    init setup

---
+++ include/FreeRTOSConfig.h
@@ -0,0 +1,6 @@
+#define configUSE_PREEMPTION 1
+#define configUSE_16_BIT_TICKS 0
+#define configMAX_PRIORITIES 5
+#define configMINIMAL_STACK_SIZE 128
+#define configTOTAL_HEAP_SIZE (10 * 1024)
+#define configTICK_RATE_HZ 1000
